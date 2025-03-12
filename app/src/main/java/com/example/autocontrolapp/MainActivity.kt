34a2e50af3a752a649243488465743511d087ae7
danisommer <danibee2005@gmail.com>
2025-03-12 20:17:45 -0300

    refactor

--- app/src/main/java/com/example/autocontrolapp/MainActivity.kt
+++ app/src/main/java/com/example/autocontrolapp/MainActivity.kt
@@ -3,14 +3,12 @@ package com.example.autocontrolapp
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.ui.AppBarConfiguration
-import com.example.autocontrolapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
 
